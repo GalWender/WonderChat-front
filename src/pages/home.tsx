4eecb96fbdcefa9c10ff62c020a369511cc8d7c9
Gal Wender <galwender@gmail.com>
2023-10-25 20:17:02 +0300

    added browser login page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,38 +7,64 @@ import HeroHangout from '../assets/svg/hero-hangout.svg?react'
 import HeroListen from '../assets/svg/hero-bird-listen.svg?react'
 import Logo from '../assets/svg/whiteLogo.svg?react'
 import { useNavigate } from 'react-router-dom';
+import { AppState } from '../interfaces/store';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+
+interface Props {
+    // isPhone: boolean;
+    // setIsPhone: (val: boolean) => void;
+    // phoneCodes: { name: string; code: string }[];
+    // handleSubmit: (e: React.FormEvent) => void;
+}
 
 export const Home = () => {
+    const device = useSelector((state: AppState) => state.userModule.isMobile);
+
+    type ComponentMap = {
+        Mobile: (props: Props) => JSX.Element;
+        Browser: (props: Props) => JSX.Element;
+    };
+
+    const cmp = useMemo<ComponentMap>(() => {
+        return {
+            Mobile,
+            Browser
+        }
+    }, [])
+
+    return <section className="home">
+        {cmp[device as keyof ComponentMap]({})}
+    </section>
+}
+
+const Mobile = () => {
     const navigate = useNavigate()
-    // const loggedinUser = useSelector((state: AppState) => state.userModule.loggedinUser);
 
-    // const handleRegister = () => {
-    //     navigate("/register")
-    // }
+    return <section className="mobile">
+        <div className="header">
+            <Logo />
+            <h4>WonderChat</h4>
+        </div>
+        <div className="svg-container">
+            <HeroChat className="svg1" />
+            <HeroHangout className="svg2" />
+            <HeroListen className="svg3" />
+        </div>
+        <div className="text-content-container">
+            <h5>Welcome to WonderChat</h5>
+            <p>Join over 100 million people who use WonderChat to talk with communities and friends.</p>
+        </div>
+        <div className="btn-container">
+            <button className="btn1" onClick={() => navigate("/register")}>Register</button>
+            <button className="btn2" onClick={() => navigate("/login")}>Login</button>
+        </div>
+    </section>
+}
 
-    // const handleLogin = () => {
-    //     navigate("/register")
-    // }
-
-    return (
-        <section className="home">
-            <div className="header">
-                <Logo/>
-                <h4>WonderChat</h4>
-            </div>
-            <div className="svg-container">
-                <HeroChat className="svg1"/>
-                <HeroHangout className="svg2"/>
-                <HeroListen className="svg3"/>
-            </div>
-            <div className="text-content-container">
-                <h5>Welcome to WonderChat</h5>
-                <p>Join over 100 million people who use WonderChat to talk with communities and friends.</p>
-            </div>
-            <div className="btn-container">
-                <button className="btn1" onClick={()=>navigate("/register")}>Register</button>
-                <button className="btn2" onClick={()=>navigate("/login")}>Login</button>
-            </div>
-        </section>
-    )
+const Browser = () => {
+    
+    return <section className="browser">
+        
+    </section>
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,65 @@
+import { useSelector } from "react-redux";
+import { AppState } from "../interfaces/store";
+import { useMemo } from "react"
+
+interface Props {
+    // isPhone: boolean;
+    // setIsPhone: (val: boolean) => void;
+    // phoneCodes: { name: string; code: string }[];
+    // handleSubmit: (e: React.FormEvent) => void;
+}
+
 export const Login = () => {
+    const device = useSelector((state: AppState) => state.userModule.isMobile);
+
+    type ComponentMap = {
+        Mobile: (props: Props) => JSX.Element;
+        Browser: (props: Props) => JSX.Element;
+    };
+
+    const cmp = useMemo<ComponentMap>(() => {
+        return {
+            Mobile,
+            Browser
+        }
+    }, [])
+
     return <section className="login">
-        
+        {cmp[device as keyof ComponentMap]({})}
     </section>
 }
+
+const Mobile = () => {
+    return <section className="mobile">
+
+    </section>
+}
+
+const Browser = () => {
+
+    const handleSubmit = () => {
+
+    }
+
+    return <section className="browser">
+        <form onSubmit={handleSubmit}>
+            <div className="header">
+                <h5>Welcome back!</h5>
+                <p>We're so excited to see you again!</p>
+            </div>
+            <div className="field">
+                <label htmlFor='email-phone'>EMAIL OR PHONE NUMBER<span className="red">*</span></label>
+                <input type="text" id='email-phone' />
+            </div>
+            <div className="field">
+                <label htmlFor='password'>PASSWORD<span className="red">*</span></label>
+                <input type="password" id='password' />
+                <a href="">Forgot your password?</a>
+            </div>
+            <div className="btn-container">
+                <button className="btn2">Log In</button>
+            </div>
+            <p>Need an account? <a href="/register">Register</a></p>
+        </form>
+    </section>
+} 
