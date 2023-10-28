7f4e1d416ec32b0a843f8be1ef67e4b6df3dc299
Gal Wender <galwender@gmail.com>
2023-10-28 18:31:06 +0300

    fixed login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,3 @@
-import { useSelector } from "react-redux";
-import { AppState } from "../interfaces/store";
-import { useMemo } from "react"
 import { BackgroundSvgs } from "../cmps/background-svgs";
 import { NavLink } from "react-router-dom";
 
@@ -12,62 +9,12 @@ interface Props {
 }
 
 export const Login = () => {
-    const device = useSelector((state: AppState) => state.userModule.isMobile);
 
-    type ComponentMap = {
-        Mobile: (props: Props) => JSX.Element;
-        Browser: (props: Props) => JSX.Element;
-    };
+    const handleSubmit = () => {
 
-    const cmp = useMemo<ComponentMap>(() => {
-        return {
-            Mobile,
-            Browser
-        }
-    }, [])
+    }
 
     return <section className="login">
-        {cmp[device as keyof ComponentMap]({})}
-    </section>
-}
-
-const Mobile = () => {
-  
-    const handleSubmit = () => {
-
-    }
-
-    return <section className="mobile">
-        {/* <BackgroundSvgs/> */}
-        <form onSubmit={handleSubmit}>
-            <div className="header">
-                <h5>Welcome back!</h5>
-                <p>We're so excited to see you again!</p>
-            </div>
-            <div className="field">
-                <label htmlFor='email-phone'>EMAIL OR PHONE NUMBER<span className="red">*</span></label>
-                <input type="text" id='email-phone' />
-            </div>
-            <div className="field">
-                <label htmlFor='password'>PASSWORD<span className="red">*</span></label>
-                <input type="password" id='password' />
-                <a href="">Forgot your password?</a>
-            </div>
-            <div className="btn-container">
-                <button className="btn2">Log In</button>
-            </div>
-            <p>Need an account? <NavLink to={"/register"}>Register</NavLink></p>
-        </form>
-    </section>
-}
-
-const Browser = () => {
-
-    const handleSubmit = () => {
-
-    }
-
-    return <section className="browser">
         <BackgroundSvgs/>
         <form onSubmit={handleSubmit}>
             <div className="header">
@@ -89,4 +36,4 @@ const Browser = () => {
             <p>Need an account? <NavLink to={"/register"}>Register</NavLink></p>
         </form>
     </section>
-} 
+}
